3c3b7fc07b7a12367027d56705228cc8a9653f0d
victor <vjose1903@outlook.es>
2020-09-01 18:05:06 -0400

    clase #19

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,25 +1,47 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 declare let $: any;
 @Component({
-  selector: "app-inicio",
-  templateUrl: "./inicio.component.html",
+  selector: 'app-inicio',
+  templateUrl: './inicio.component.html',
   styles: [],
 })
 export class InicioComponent implements OnInit {
   mostrarYo = true;
-  constructor() {}
+  constructor(private router: Router) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    window.scrollTo(0, 0);
+    this.tooltip();
+  }
 
   yoMostrar() {
     this.mostrarYo = !this.mostrarYo;
   }
 
   tecnologias() {
-    $("#modalTecnologias").modal();
+    $('#modalTecnologias').modal();
   }
 
   sobreMi() {
-    $("#sobreMi").modal();
+    $('#sobreMi').modal();
+  }
+
+  mostrarNoticia() {
+    this.tooltip(true);
+
+    setTimeout(() => {
+      this.router.navigateByUrl('noticiaCompleta');
+    }, 150);
+  }
+
+  tooltip(cerrar = false) {
+    if (cerrar) {
+      $('[data-toggle="tooltip"]').tooltip('hide');
+    } else {
+      $(() => {
+        $('[data-toggle="tooltip"]').tooltip();
+      });
+    }
   }
 }
